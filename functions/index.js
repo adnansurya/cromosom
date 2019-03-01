9a9bf0e39edc99bc75b7aa85b74c5e76e5e05255
Unknown <pc.keyboardist@gmail.com>
2019-03-02 06:14:59 +0800

    add routing express js

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+//include library expressjs setelah install di folder functions : npm i --save firebase-functions
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//set template engine ke ejs setelah install di folder functions : npm install ejs -save
+app.set('view engine', 'ejs');
+//set folder static ke public untuk menyimpan file statis
+app.use(express.static(__dirname + '/public'));
+
+app.get('/tes', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("index");
+});
+
+
+exports.apps = functions.https.onRequest(app);
