64feedc6b8e64465d0f856cc5cd5700183df913c
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-04-01 23:54:08 +0800

    routing express js

--- functions/index.js
+++ functions/index.js
@@ -8,9 +8,34 @@ app.set('view engine', 'ejs');
 //set folder static ke public untuk menyimpan file statis
 app.use(express.static(__dirname + '/public'));
 
-app.get('/tes', function(req,res) {
+app.get('/', function(req,res) {
     //render file ejs dari direktori functions/views
     res.render("index");
+});
+
+app.get('/work', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("work");
+});
+
+app.get('/about', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("about");
+});
+
+app.get('/workdetail', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("workdetail");
+});
+
+app.get('/funwork', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("funwork");
+});
+
+app.get('/contact', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("contact");
 });
 
 
