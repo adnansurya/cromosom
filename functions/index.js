7fceb57f784c5b38e899156867d5370916fe3c6d
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-04-25 18:01:07 +0800

    edit about and contact via js

--- functions/index.js
+++ functions/index.js
@@ -6,36 +6,36 @@ const app = express();
 //set template engine ke ejs setelah install di folder functions : npm install ejs -save
 app.set('view engine', 'ejs');
 //set folder static ke public untuk menyimpan file statis
-app.use(express.static(__dirname + '/public'));
+app.use('/public',express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("index", {page : 'index'});
+    res.render("index", {page : 'Home'});
 });
 
 app.get('/work', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("work", {page:'work'});
+    res.render("work", {page:'Work'});
 });
 
 app.get('/about', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("about", {page:'about'});
+    res.render("about", {page:'About'});
 });
 
 app.get('/workdetail', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("workdetail", {page : 'workdetail'});
+    res.render("workdetail", {page : 'Workdetail'});
 });
 
 app.get('/funwork', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("funwork", {page:'funwork'});
+    res.render("funwork", {page:'Funwork'});
 });
 
 app.get('/contact', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("contact", {page:'contact'});
+    res.render("contact", {page:'Contact'});
 });
 
 app.get('/login', function(req,res) {
@@ -51,6 +51,15 @@ app.get('/admin', function(req,res) {
 app.get('/admin_edit_footer', function(req,res) {
     //render file ejs dari direktori functions/views
     res.render("admin_edit_footer");
+});
+
+app.get('/admin_edit_about', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("admin_edit_about");
+});
+app.get('/admin_edit_contact', function(req,res) {
+    //render file ejs dari direktori functions/views
+    res.render("admin_edit_contact");
 });
 
 
