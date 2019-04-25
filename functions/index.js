49692fd14849cfec01aa6e7c186d61ccd298e664
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2019-04-25 19:07:38 +0800

    view about and contact via express

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,88 @@
 const functions = require('firebase-functions');
 //include library expressjs setelah install di folder functions : npm i --save firebase-functions
 const express = require('express');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const db = admin.database();
 const app = express();
+
+
 
 //set template engine ke ejs setelah install di folder functions : npm install ejs -save
 app.set('view engine', 'ejs');
 //set folder static ke public untuk menyimpan file statis
 app.use('/public',express.static(__dirname + '/public'));
 
+
+
+let footer;
+
 app.get('/', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("index", {page : 'Home'});
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("index", {page : 'Home', footer: footer});
 });
 
 app.get('/work', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("work", {page:'Work'});
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("work", {page:'Work', footer : footer});
 });
 
+let about;
 app.get('/about', function(req,res) {
+   
     //render file ejs dari direktori functions/views
-    res.render("about", {page:'About'});
+    db.ref("about").once("value").then(function(snapshot) {
+        about = snapshot.val();
+        
+    });
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("about", {page:'About',footer : footer, about : about});
+   
+   
+    
 });
 
 app.get('/workdetail', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("workdetail", {page : 'Workdetail'});
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("workdetail", {page : 'Workdetail', footer : footer});
 });
 
 app.get('/funwork', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("funwork", {page:'Funwork'});
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("funwork", {page:'Funwork', footer: footer});
 });
 
+let contact;
 app.get('/contact', function(req,res) {
     //render file ejs dari direktori functions/views
-    res.render("contact", {page:'Contact'});
+    db.ref("contact").once("value").then(function(snapshot) {
+        contact = snapshot.val();
+        
+    });
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
+
+    res.render("contact", {page:'Contact', footer:footer, contact:contact});
 });
 
 app.get('/login', function(req,res) {
@@ -50,6 +97,9 @@ app.get('/admin', function(req,res) {
 
 app.get('/admin_edit_footer', function(req,res) {
     //render file ejs dari direktori functions/views
+    db.ref("footer").once("value").then(function(snapshot) {
+        footer = snapshot.val();
+    });
     res.render("admin_edit_footer");
 });
 
